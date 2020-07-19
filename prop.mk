f47d3cb9a9c47885c12e2ab66e83352a05b93401
scarymen1325 <kyslenko3@outlook.com>
2020-07-19 23:18:56 +0000

    daisy: set another camera props

--- prop.mk
+++ prop.mk
@@ -24,3 +24,22 @@ persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.camera.HAL3.enabled=1 \
 persist.vendor.camera.CDS=off
+
+# Camera hal buffer management
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.managebuffer.enable=1
+
+#Temporal Noise Reduction
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.tnr_cds=1 \
+persist.camera.tnr.video=1 \
+persist.vendor.camera.tnr.video=1 \
+persist.camera.tnr.preview=1 \
+persist.vendor.camera.tnr.preview=1 \
+persist.camera.tnr.snapshot=1 \
+persist.vendor.camera.tnr.snapshot=1 \
+persist.camera.llnoise=1 \
+persist.tnr.process.plates=1 \
+persist.vendor.tnr.process.plates=1 \
+persist.denoise.process.plates=1 \
+persist.vendor.denoise.process.plates=1
