1b4416f80a0969b9b8378e57b74d6108c78764bd
vitor00almei <pondebatata@outlook.com>
2020-08-08 21:36:08 -0300

    daisy: pick Xiaomi Parts from ginkgo

--- device.mk
+++ device.mk
@@ -79,6 +79,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Device-Specific
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Properties
 -include device/xiaomi/daisy/prop.mk
 
